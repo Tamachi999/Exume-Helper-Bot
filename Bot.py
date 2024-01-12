6afdd31126612a824636220150ef70d1234166cb
Tamachi999 <87845441+Tamachi999@users.noreply.github.com>
2024-01-12 17:43:34 -0500

    Update Bot.py
    Agregado base de datos de estadísticas de personajes.

--- Bot.py
+++ Bot.py
@@ -1,20 +1,28 @@
 #by Soul999
 import telebot
-from telebot import types
+import json
 import random
 
 # Define los ID de los administradores, reemplaza con los ID de tus administradores
-administradores = [112638494, 7727189]
+administradores = [796054522, 7727189]
 
-bot = telebot.TeleBot("TOKEN AQUI")
+bot = telebot.TeleBot("6715500431:AAHZ1FHsYXZGilBgYaz_axdNK_5UJKMUv88")
 
-estadisticas = {}
+# Nombre del archivo donde se almacenarán las estadísticas
+archivo_estadisticas = "estadisticas.json"
+
+# Cargar estadísticas desde el archivo (si existe)
+try:
+    with open(archivo_estadisticas, "r") as file:
+        estadisticas = json.load(file)
+except FileNotFoundError:
+    estadisticas = {}
 
 # Restringe el uso de comandos solo a administradores
 def es_administrador(user_id):
     return user_id in administradores
 
-def generar_estadisticas():
+def generar_estadisticas(nombre_usuario):
     fuerza = destreza = voluntad = 0
     
     # Genera estadísticas evitando 1 o 6 en cada tirada
@@ -37,27 +45,24 @@ def generar_estadisticas():
     else:
         tipo_aventurero = "Mago"
 
-    return {"Clase": tipo_aventurero, "PV": puntos_vida, "FUE": fuerza, "DES": destreza, "VOL": voluntad}
+    return {"Nombre de usuario": nombre_usuario, "Clase": tipo_aventurero, "PV": puntos_vida, "FUE": fuerza, "DES": destreza, "VOL": voluntad}
 
-def disminuir_estadistica(nombre_usuario, estadistica, cantidad):
-    if nombre_usuario in estadisticas:
-        if estadistica in estadisticas[nombre_usuario] and cantidad > 0:
-            estadisticas[nombre_usuario][estadistica] -= cantidad
-            return True
-    return False
-
-@bot.message_handler(commands=['start', 'help'])
-def iniciar(mensaje):
-    bot.send_message(mensaje.chat.id, 'Bienvenido al bot de estadísticas de personajes.')
+def tirar_dado():
+    return random.randint(1, 6)
 
 @bot.message_handler(commands=['generar'])
 def generar(mensaje):
     nombre_usuario = mensaje.from_user.username
-    estadisticas_usuario = generar_estadisticas()
+    estadisticas_usuario = generar_estadisticas(nombre_usuario)
     estadisticas[nombre_usuario] = estadisticas_usuario
+
+    # Guardar las estadísticas en el archivo
+    with open(archivo_estadisticas, "w") as file:
+        json.dump(estadisticas, file)
 
     # Formatear las estadísticas horizontalmente
     mensaje_respuesta = (
+        f'Nombre de usuario: {estadisticas_usuario["Nombre de usuario"]}\n'
         f'Clase: {estadisticas_usuario["Clase"]}\n'
         f'PV: {estadisticas_usuario["PV"]}\n'
         f'FUE: {estadisticas_usuario["FUE"]}\n'
@@ -76,6 +81,7 @@ def obtener(mensaje):
     else:
         # Formatear las estadísticas horizontalmente
         mensaje_respuesta = (
+            f'Nombre de usuario: {estadisticas_usuario["Nombre de usuario"]}\n'
             f'Clase: {estadisticas_usuario["Clase"]}\n'
             f'PV: {estadisticas_usuario["PV"]}\n'
             f'FUE: {estadisticas_usuario["FUE"]}\n'
@@ -87,15 +93,11 @@ def obtener(mensaje):
 
 @bot.message_handler(commands=['tirar_dado'])
 def tirar_dado_command(mensaje):
-    tirar_dado(mensaje)
-
-def tirar_dado(mensaje):
-    resultado = random.randint(1, 6)
-    bot.send_message(mensaje.chat.id, 'El resultado de la tirada es: ' + str(resultado))
+    resultado = tirar_dado()
+    bot.send_message(mensaje.chat.id, f'El resultado de la tirada es: {resultado}')
 
 @bot.message_handler(commands=['disminuir'])
 def disminuir_estadistica_command(mensaje):
-    # Ejemplo de uso: /disminuir usuario FUE 1
     partes = mensaje.text.split(' ')
     if len(partes) == 4:
         nombre_usuario = partes[1]
@@ -104,6 +106,10 @@ def disminuir_estadistica_command(mensaje):
         if es_administrador(mensaje.from_user.id):
             if disminuir_estadistica(nombre_usuario, estadistica, cantidad):
                 bot.send_message(mensaje.chat.id, f'{estadistica} disminuida en {cantidad} para {nombre_usuario}.')
+
+                # Guardar las estadísticas en el archivo
+                with open(archivo_estadisticas, "w") as file:
+                    json.dump(estadisticas, file)
             else:
                 bot.send_message(mensaje.chat.id, 'Error al disminuir la estadística. Asegúrate de especificar una estadística válida y una cantidad mayor que 0.')
         else:
